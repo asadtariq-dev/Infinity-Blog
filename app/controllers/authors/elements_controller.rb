c1a8a76244f6b2e891a822c20ef0d894ed712583
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-25 02:48:12 +0500

    Updated ElementsController
    * Removed index, show, new and edit actions
    * Updated create action
    * Added private method set_post.

--- app/controllers/authors/elements_controller.rb
+++ app/controllers/authors/elements_controller.rb
@@ -1,49 +1,23 @@
 module Authors
   class ElementsController < AuthorsController
-    before_action :set_element, only: %i[show edit update destroy]
-
-    # GET /elements or /elements.json
-    def index
-      @elements = Element.all
-    end
-
-    # GET /elements/1 or /elements/1.json
-    def show; end
-
-    # GET /elements/new
-    def new
-      @element = Element.new
-    end
-
-    # GET /elements/1/edit
-    def edit; end
+    before_action :set_post
+    before_action :set_element, only: %i[update destroy]
 
     # POST /elements or /elements.json
     def create
-      @element = Element.new(element_params)
+      @element = @post.elements.build(element_params)
 
-      respond_to do |format|
-        if @element.save
-          format.html { redirect_to element_url(@element), notice: 'Element was successfully created.' }
-          format.json { render :show, status: :created, location: @element }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @element.errors, status: :unprocessable_entity }
-        end
-      end
+      notice = if @element.save
+                 nil
+               else
+                 @element.errors.full_messages.join('.') << '.'
+               end
+      redirect_to edit_post_path(@post), notice: notice
     end
 
     # PATCH/PUT /elements/1 or /elements/1.json
     def update
-      respond_to do |format|
-        if @element.update(element_params)
-          format.html { redirect_to element_url(@element), notice: 'Element was successfully updated.' }
-          format.json { render :show, status: :ok, location: @element }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @element.errors, status: :unprocessable_entity }
-        end
-      end
+      redirect_to edit_post_path(@element.post) if @element.update(element_params)
     end
 
     # DELETE /elements/1 or /elements/1.json
@@ -58,14 +32,18 @@ module Authors
 
     private
 
+    def set_post
+      @post = current_author.posts.find(params[:post_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_element
-      @element = Element.find(params[:id])
+      @element = @post.elements.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def element_params
-      params.require(:element).permit(:element_type, :content, :post_id, :position)
+      params.require(:element).permit(:element_type, :content)
     end
   end
 end
