1d35b25b6971074591ba288491aa2cf643860779
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-27 02:13:25 +0500

    HomeController
    * Added scopes for published posts and recent posts
    * Created view for showing published posts

--- app/controllers/readers/home_controller.rb
+++ app/controllers/readers/home_controller.rb
@@ -1,6 +1,7 @@
 module Readers
-class HomeController < ReadersController
-  def index
+  class HomeController < ReadersController
+    def index
+      @posts = Post.published.most_recent_posts
+    end
   end
-end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ApplicationRecord
 
   validates :title, :description, presence: true
   validates_length_of :description, within: 20..200
+
+  scope :published, -> do
+    where(published: true)
+  end
+
+  scope :most_recent_posts, -> do
+   order(published_at: :desc)
+  end
 end
