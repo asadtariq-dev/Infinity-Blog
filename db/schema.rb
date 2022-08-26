dfa4410ba8a7550558a736d6b2d5a300677847f3
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-26 16:32:47 +0500

    After dropping element table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_215734) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_200351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,15 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_215734) do
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
   end
 
-  create_table "elements", force: :cascade do |t|
-    t.string "element_type"
-    t.bigint "post_id", null: false
-    t.integer "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_elements_on_post_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -81,11 +72,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_215734) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "elements", "posts"
   add_foreign_key "posts", "authors"
 end
