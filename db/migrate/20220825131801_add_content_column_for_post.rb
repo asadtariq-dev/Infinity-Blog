af5815926587673f8823aecba65eaac29dafd084
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-26 16:30:55 +0500

    Created Migration
    * Add content column for post

---
+++ db/migrate/20220825131801_add_content_column_for_post.rb
@@ -0,0 +1,6 @@
+class AddContentColumnForPost < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :content, :text
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
