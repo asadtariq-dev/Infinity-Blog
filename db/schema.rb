7d9e081d2c231938d8952f561a4686d5b0d86cdf
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-27 06:27:24 +0500

    Added migration
    * To add post_id as FK in like
    * To add author_id as FK in like

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_200351) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_002443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_200351) do
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id", "post_id"], name: "index_likes_on_author_id_and_post_id", unique: true
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -78,5 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_200351) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "likes", "authors"
+  add_foreign_key "likes", "posts"
   add_foreign_key "posts", "authors"
 end
