11975582d17127eb757c2244096b921034a6088e
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-28 05:28:08 +0500

    Added JQuery for reply
    * Show reply textfield when reply button is clicked

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "trix"
-import "@rails/actiontext"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "trix";
+import "@rails/actiontext";
+
+// Show textarea after clicking the reply button in post comments
+document.querySelectorAll(".comment-form-display").forEach((el) => {
+  el.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    el.nextElementSibling.style = "display: block;";
+  });
+});
