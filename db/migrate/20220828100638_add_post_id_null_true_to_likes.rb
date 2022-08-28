6bc56bdb11c8ba52462d8438bc62fa2a6c0cb5fc
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-28 17:20:38 +0500

    Added Migration
    * Add post_id for like to be null=true

---
+++ db/migrate/20220828100638_add_post_id_null_true_to_likes.rb
@@ -0,0 +1,5 @@
+class AddPostIdNullTrueToLikes < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :likes, :post_id, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_28_092319) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_28_100638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,7 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_28_092319) do
 
   create_table "likes", force: :cascade do |t|
     t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "comment_id"
