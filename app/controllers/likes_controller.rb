2432973f5420261c111e9a06827078e9331eb79a
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-29 01:13:25 +0500

    Added Likes to a comment

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,20 +3,22 @@ class LikesController < ApplicationController
     @like = current_author.likes.new(like_params)
 
     flash[:notice] = @like.errors.full_messages.to_sentence unless @like.save
-    redirect_to @like.post
+
+    redirect_to @like.post if @like.comment.nil?
+    redirect_to @like.comment if @like.post.nil?
   end
 
   def destroy
     @like = current_author.likes.find(params[:id])
-    post = @like.post
 
     @like.destroy
-    redirect_to post
+    redirect_to @like.post if @like.comment.nil?
+    redirect_to @like.comment if @like.post.nil?
   end
 
   private
 
   def like_params
-    params.require(:like).permit(:post_id)
+    params.require(:like).permit(:post_id, :comment_id)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  validates :author_id, uniqueness: { scope: :post_id }
+  # validates :author_id, uniqueness: { scope: :post_id }
   belongs_to :author
   belongs_to :post, optional: true
   belongs_to :comment, optional: true
