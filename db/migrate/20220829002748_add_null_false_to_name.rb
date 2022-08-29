f294a290a0c657caf0026829cca766defea0b698
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-29 07:51:39 +0500

    Managed Devise
    * Styled the devise views
    * Added first_name and last_name fields to devise author model and forms
    * Added style to Login page
    * Added style to Signup page
    * Added style to Forgot Password page
    * Added style to Edit profie

---
+++ db/migrate/20220829002748_add_null_false_to_name.rb
@@ -0,0 +1,6 @@
+class AddNullFalseToName < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :authors, :first_name, false
+    change_column_null :authors, :last_name, false
+  end
+end
