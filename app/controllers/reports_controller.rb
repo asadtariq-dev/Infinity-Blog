b248892dda924c3f1c431c215c46114282a34499
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-30 23:09:20 +0500

    Report Post
    * removed the reported attribute of post

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,8 +2,6 @@ class ReportsController < ApplicationController
   def create
     @report = current_author.reports.new(report_params)
 
-    Post.find(report_params[:post_id]).update_attribute(:reported, true)
-
     flash[:notice] = @report.errors.full_messages.to_sentence unless @report.save
 
     redirect_to @report.post if @report.comment.nil?
@@ -14,7 +12,6 @@ class ReportsController < ApplicationController
     @report = current_author.reports.find(params[:id])
 
     @report.destroy
-    Post.find(report_params[:post_id]).update_attribute(:reported, false) if Post.joins(:reports).nil?
     redirect_to @report.post if @report.comment.nil?
     redirect_to @report.comment if @report.post.nil?
   end
