91a0cc0844ea1d9bcb0b1ca9b408382813f7d1c3
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-30 16:51:47 +0500

    Posts Controller
    * Ordered posts in desc order
    * Removed publish action
    * Added submit action

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -1,10 +1,10 @@
 module Authors
   class PostsController < AuthorsController
-    before_action :set_post, only: %i[show edit update destroy publish]
+    before_action :set_post, only: %i[show edit update destroy submit]
 
     # GET /posts or /posts.json
     def index
-      @posts = current_author.posts
+      @posts = current_author.posts.order(published_at: :desc)
     end
 
     def show
@@ -34,19 +34,22 @@ module Authors
       redirect_to edit_post_path(@post) if @post.update(post_params)
     end
 
-    def publish
+    def submit
       @post = current_author.posts.find(params[:id])
       if @post.content.present?
         notice = nil
-        if @post.published?
+        if @post.submitted?
+          @post.update_attribute(:submitted, false)
           @post.update_attribute(:published, false)
-          @post.update_attribute(:published_at, nil)
+          @post.update_attribute(:pending, false)
+          @post.update_attribute(:submitted_at, nil)
         else
-          @post.update_attribute(:published, true)
-          @post.update_attribute(:published_at, Time.now)
+          @post.update_attribute(:submitted, true)
+          @post.update_attribute(:pending, true)
+          @post.update_attribute(:submitted_at, Time.now)
         end
       else
-        notice = 'Please Add Some Content To Publish'
+        notice = 'Please Add Some Content To Submit'
       end
       redirect_to edit_post_path(@post), notice: notice
     end
@@ -65,7 +68,7 @@ module Authors
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :content, :header_image, :published, :published_at)
+      params.require(:post).permit(:title, :description, :content, :header_image, :submitted, :submitted_at)
     end
   end
 end
