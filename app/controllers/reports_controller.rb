020a38dab191b2fdb0211a840a20f9960ec7326d
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-30 23:02:22 +0500

    Report Post
    * Update the reported attribute of post when post is reported

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,8 @@ class ReportsController < ApplicationController
   def create
     @report = current_author.reports.new(report_params)
 
+    Post.find(report_params[:post_id]).update_attribute(:reported, true)
+
     flash[:notice] = @report.errors.full_messages.to_sentence unless @report.save
 
     redirect_to @report.post if @report.comment.nil?
@@ -12,6 +14,7 @@ class ReportsController < ApplicationController
     @report = current_author.reports.find(params[:id])
 
     @report.destroy
+    Post.find(report_params[:post_id]).update_attribute(:reported, false) if Post.joins(:reports).nil?
     redirect_to @report.post if @report.comment.nil?
     redirect_to @report.comment if @report.post.nil?
   end
