e4efd4243edba836965f9f10a184df31839c77e0
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-31 00:38:06 +0500

    Added admin_column to authors model

---
+++ db/migrate/20220830191608_add_admin_column_to_author.rb
@@ -0,0 +1,6 @@
+class AddAdminColumnToAuthor < ActiveRecord::Migration[7.0]
+  def change
+    add_column :authors, :admin, :boolean
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_180519) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_191608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_180519) do
     t.integer "failed_attempts", default: 0, null: false
     t.datetime "locked_at"
     t.string "unlock_token"
+    t.boolean "admin"
     t.index ["email"], name: "index_authors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_authors_on_unlock_token", unique: true
