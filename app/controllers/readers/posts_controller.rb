d30c41fd12b33cd9fe05a3d6c2f8fde4deafd21e
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-31 18:19:31 +0500

    Added redirect to home if post is not available

--- app/controllers/readers/posts_controller.rb
+++ app/controllers/readers/posts_controller.rb
@@ -1,7 +1,12 @@
 module Readers
   class PostsController < ReadersController
     def show
-      @post = Post.published.find(params[:id])
+      @post = Post.find(params[:id])
+      if @post.published?
+        @post = Post.find(params[:id])
+      else
+        redirect_to root_path, notice: 'Post has been unpublished'
+      end
     end
   end
 end
