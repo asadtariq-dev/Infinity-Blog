f48ce38506e66b843de55f5f776f55c26c3e1aa7
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-31 18:18:02 +0500

    Moved author and reader layout to layout dir

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,6 @@
 class AuthorsController < ApplicationController
   before_action :authenticate_author!
 
-  layout 'users/authors'
-
   def show
     @posts = current_author.posts.order(published_at: :desc)
   end
--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,3 +1,2 @@
 class ReadersController < ApplicationController
-  layout 'users/readers'
 end
