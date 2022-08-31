71131c205297296ef83dfa831653409887df43a1
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-31 05:44:37 +0500

    Added Confirmation strategy for Author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_191608) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_212558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_191608) do
     t.datetime "locked_at"
     t.string "unlock_token"
     t.boolean "admin"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_authors_on_confirmation_token", unique: true
     t.index ["email"], name: "index_authors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_authors_on_unlock_token", unique: true
