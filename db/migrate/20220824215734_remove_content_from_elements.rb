e3de6f00b83b48e39be1c30ae61c9e34dc5d977f
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-05 23:02:35 +0500

    removed unused migration

--- db/migrate/20220824215734_remove_content_from_elements.rb
+++
@@ -1,8 +0,0 @@
-class RemoveContentFromElements < ActiveRecord::Migration[7.0]
-  def up
-    remove_column :elements, :content
-  end
-  def down
-    add_column :elements, :content, :text
-  end
-end
--- db/migrate/20220825200351_drop_elements_table.rb
+++
@@ -1,8 +0,0 @@
-class DropElementsTable < ActiveRecord::Migration[7.0]
-  def up
-    drop_table :elements
-  end
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
