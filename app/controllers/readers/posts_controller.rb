7bb416c051e513989bd44512d1bd6b5166b03b09
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-05 17:40:02 +0500

    Resolved issue for double assignment

--- app/controllers/readers/posts_controller.rb
+++ app/controllers/readers/posts_controller.rb
@@ -1,7 +1,6 @@
 module Readers
   class PostsController < ReadersController
     def show
-      @post = Post.find(params[:id])
       if @post.published?
         @post = Post.find(params[:id])
       else
