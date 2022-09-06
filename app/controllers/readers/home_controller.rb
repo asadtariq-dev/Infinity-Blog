97443da7651b66110377dcfaacb35a40246c7036
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-06 14:18:04 +0500

    Added comments section

--- app/controllers/readers/home_controller.rb
+++ app/controllers/readers/home_controller.rb
@@ -2,6 +2,7 @@ module Readers
   class HomeController < ReadersController
     def index
       @posts = Post.published.most_recent_posts
+      @comments = Comment.order(id: :desc).limit(5)
     end
   end
 end
