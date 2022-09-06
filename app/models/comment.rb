bf9f423abe8c1110bf5563255030c1aa1abc4984
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-06 17:00:27 +0500

    Added dependent destroy to comment and reply

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author
   belongs_to :parent, class_name: 'Comment', optional: true
-  has_many :comments, foreign_key: :parent_id
+  has_many :comments, foreign_key: :parent_id, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :reports, dependent: :destroy
 
