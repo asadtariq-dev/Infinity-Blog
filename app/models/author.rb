991177e1674e23b7362ff67073fc529f63f73f60
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-16 17:01:12 +0500

    Modified

--- app/models/author.rb
+++ app/models/author.rb
@@ -11,7 +11,7 @@ class Author < ApplicationRecord
   validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true, format: { with: Devise.email_regexp }
   validates :first_name, :last_name, length: { within: 3..10 }
-  validates :password, length: { within: 7..12 }
+  validates :password, length: { within: 6..12 }
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
