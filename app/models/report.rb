b82f99c35f79c3631c9b5d55604906caeeaec544
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-18 03:24:46 +0500

    Arranged the code

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  validates :author_id, uniqueness: { scope: %i[reportable_id reportable_type] }
-
   belongs_to :author
   belongs_to :reportable, polymorphic: true
+
+  validates :author_id, uniqueness: { scope: %i[reportable_id reportable_type] }
 end
