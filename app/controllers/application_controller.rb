6fdd0268a8b11028067637684350d0365eb5aa18
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-30 22:40:36 +0500

    Added cors config
    * Commented out devise authentication to allow api requests
    * Added cors headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,31 @@
 
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
-  before_action :authenticate_author!
 
+  # before_action :authenticate_author! ## to allow api request
+
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActionController::RoutingError, with: :routing_error
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = '1728000'
+  end
+
+  def cors_preflight_check
+    return if request.method == :options
+
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 
   protected
 
