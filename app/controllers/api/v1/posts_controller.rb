71a4b6186cc092641fb921179e9d8d6e8f4af142
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-04 17:24:51 +0500

    Added show action and route

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -8,6 +8,12 @@ module Api
         @json_posts = @posts.map { |e| e.as_json.merge(header_image: e.header_image.url, content: e.content, author: { first_name: e.author.first_name, last_name: e.author.last_name }) }
         render json: @json_posts
       end
+
+      def show
+        @post = Post.find(params[:id])
+        @json_post = @post.as_json.merge(header_image: @post.header_image.url, content: @post.content, author: { first_name: @post.author.first_name, last_name: @post.author.last_name })
+        render json: @json_post
+      end
     end
   end
 end
