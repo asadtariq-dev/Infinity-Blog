749c3f480d09e2f725b77cb41b55b390b642664b
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-04 20:14:28 +0500

    Added Serialization before sending json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include ActionController::Serialization
   include Pundit::Authorization
 
   # before_action :authenticate_author! ## to allow api request
